e746c75062b2dcb95941772d762f41837431933f
Raghu <gorur.raghu@gmail.com>
2021-01-09 17:00:27 +0530

    First Commit

---
+++ dictionarytest..py
@@ -0,0 +1,9 @@
+# %%
+print ("Hello world");
+num1=int(input("First number"));
+num2=int(input("Second number"));
+print str(num1 + num2);
+i = 10;
+while i > num1:
+    print(i);
+    i += 1;
---
+++ fib.py
@@ -0,0 +1,14 @@
+print ("HELLO WORLD")
+prev = 1
+curr = 1
+next = 0
+
+i=1
+print (curr)
+print(prev)
+while i > 0:
+    next = curr + prev
+    print(next);
+    prev=curr
+    curr=next
+    i += 1;
